5314d2ad8c30c142bcb483e7825d0e3b397843fb
Matthias Popp <matthias.poqq@gmail.com>
2021-05-12 17:07:23 +0200

    init commit

---
+++ BrokenConnection/CustomDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrokenConnection
+{
+  public class CustomDbContext : DbContext
+  {
+    public DbSet<Projects> Projects { get; set; }
+
+    public CustomDbContext(DbContextOptions options) : base(options)
+    {
+    }
+  }
+
+  public class Projects
+  {
+    public int Id { get; set; }
+  }
+}
